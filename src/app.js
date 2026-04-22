6aa8e9e704b100d57d1720ff8fdcf1e2e0b4b2b6
BV-Works <maildinerodeinternet@gmail.com>
2026-04-22 17:09:12 +0200

    jrs-primera vista

--- src/app.js
+++ src/app.js
@@ -15,18 +15,27 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const openapiDocument = YAML.load(path.join(process.cwd(), "docs", "openapi.yaml"));
 
+// Motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
 // Middlewares
 app.use(morgan("dev")); // console.log de las peticiones al servidor para facilitar el desarrollo y debugging
 app.use(helmet()); // Securización de cabeceras HTTP
 app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // permitir a express leer formularios html
 app.use(cookieParser());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiDocument));
+
+
+// Esto es FUNDAMENTAL porque: EJS genera HTML pero el navegador necesita:CSS y js e imagenes
+app.use(express.static('./src/public'));
 
 // Rutas
 const authRoutes = require("./routes/auth.routes");
 const favoritesRoutes = require("./routes/favorites.routes");
 const usersRoutes = require("./routes/users.routes");
 const filmsRoutes = require("./routes/films.routes");
+const viewRoutes = require("./routes/view.routes");
 
 app.get("/", (_req, res) => {
   res.json({ message: "funciona" });
@@ -36,6 +45,7 @@ app.use("/api", authRoutes);
 app.use("/api", favoritesRoutes);
 app.use("/api", usersRoutes);
 app.use("/api", filmsRoutes);
+app.use("/", viewRoutes);
 
 // 404
 app.use((req, res) => {
